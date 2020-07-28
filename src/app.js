aefd0a03c54ceffda6fcfe531d843db07508f9a8
Stephen <stephen_h369@live.co.uk>
2020-07-28 15:07:32 +0100

    total population now displays

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el: '#app',
+        data: {
+            countries: []
+        },
+
+        mounted: function() {
+            this.fetchCountries();
+        },
+
+        computed:{
+            totalPopulation: function() {
+                console.log(this.countries);
+                return this.countries.reduce((total, country) => total + country.population, 0);
+            }            
+        },
+
+        methods: {
+            fetchCountries: function() {
+                fetch("https://restcountries.eu/rest/v2/all")
+                .then(response => response.json())
+                .then(data => this.countries = data);
+            }
+        }
+    });
+});
